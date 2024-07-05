8f58bf113d4b1f4f7ae286a235c3d58b30eb713b
HariHaran2229-dev <v.m.hariharan.mano@gmail.com>
2024-07-05 15:36:09 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -99,34 +99,7 @@ import java.io.*;
 			vtx2.nbrs.remove(vname1);
 		}
 
-		public void display_Map() 
-		{
-			System.out.println("\t Delhi Metro Map");
-			System.out.println("\t------------------");
-			System.out.println("----------------------------------------------------\n");
-			ArrayList<String> keys = new ArrayList<>(vtces.keySet());
-
-			for (String key : keys) 
-			{
-				String str = key + " =>\n";
-				Vertex vtx = vtces.get(key);
-				ArrayList<String> vtxnbrs = new ArrayList<>(vtx.nbrs.keySet());
-				
-				for (String nbr : vtxnbrs)
-				{
-					str = str + "\t" + nbr + "\t";
-                    			if (nbr.length()<16)
-                    			str = str + "\t";
-                    			if (nbr.length()<8)
-                    			str = str + "\t";
-                    			str = str + vtx.nbrs.get(nbr) + "\n";
-				}
-				System.out.println(str);
-			}
-			System.out.println("\t------------------");
-			System.out.println("---------------------------------------------------\n");
-
-		}
+		
 		
 		public void display_Stations() 
 		{
